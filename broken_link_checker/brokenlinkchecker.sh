7b87915731901bda4dd4ae99399474339a5fc9a6
Josh Espinoza <joshespi@gmial.com>
2020-02-26 07:31:50 -0700

    rsync the results to the main server

--- broken_link_checker/brokenlinkchecker.sh
+++ broken_link_checker/brokenlinkchecker.sh
@@ -6,6 +6,9 @@ sites=/var/www
 grep -REo --exclude-dir={assets,icons} --exclude='*.png' --exclude='*.svg' --exclude='*.pdf' --exclude='*.jpg' "(http|https)://[a-zA-Z0-9./?=_-:]*" $sites/*/public_html/wp-content/themes/ | sed 's/:/ > /'| perl -pe 's/\n/ > /' >  linksfound.txt
 
 #echo $sites/*/wp-content/themes/
+
 /usr/bin/php check_links.php
+
+rsync -az /home/joshe/broken_link_checker/results.txt /var/www/provo.edu/public_html/wp-content/uploads/broken-link-checker/mainweb.txt
 
 #https://unix.stackexchange.com/questions/181254/how-to-use-grep-and-cut-in-script-to-obtain-website-urls-from-an-html-file
