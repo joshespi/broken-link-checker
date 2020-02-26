cc017631e993ad87173fdb9b2049c862e3881b77
Josh Espinoza <joshespi@gmial.com>
2020-02-26 07:30:17 -0700

    output results to file

--- broken_link_checker/check_links.php
+++ broken_link_checker/check_links.php
@@ -30,10 +30,14 @@
 					
 		$i++;
 	}
+	file_put_contents($results_file, 'Links Found:' . count($links_array) . PHP_EOL, FILE_APPEND | LOCK_EX);
+	
 	//print_r($links_array);
 	foreach($links_array as $object) {
 		$check_url_status = check_url($object[1]);
 		if($check_url_status == '200') {
+		} elseif ($check_url_status == '405') {
+			
 		} else {
 			$broken_link = 'file-> ' . $object[0] . ' url-> ' . $object[1] . PHP_EOL;
 			file_put_contents($results_file, $broken_link, FILE_APPEND | LOCK_EX);
